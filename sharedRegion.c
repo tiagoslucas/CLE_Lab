2ae6d586fed5dc56e28de5f28f024344388b1bcc
FranciscoSantos97 <56774420+FranciscoSantos97@users.noreply.github.com>
2020-04-01 18:09:09 +0100

    Add files via upload

---
+++ sharedRegion.c
@@ -0,0 +1,149 @@
+/**
+ *  \file fifo.c (implementation file)
+ *
+ *  \brief Problem name: Producers / Consumers.
+ *
+ *  Synchronization based on monitors.
+ *  Both threads and the monitor are implemented using the pthread library which enables the creation of a
+ *  monitor of the Lampson / Redell type.
+ *
+ *  Data transfer region implemented as a monitor.
+ *
+ *  Definition of the operations carried out by the producers / consumers:
+ *     \li putVal
+ *     \li getVal.
+ *
+ *  \author Francisco Gonçalves and Tiago Lucas - March 2019
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <errno.h>
+
+#include "probConst.h"
+
+/** \brief producer threads return status array */
+extern int statusWorkers[NUMB_THREADS];
+
+/** \brief names of files to process */
+const char* filesToProcess;
+
+/** \brief number of files to process */
+unsigned int numbFiles;
+
+/** \brief file pointer */
+unsigned int fileId;
+
+/** \brief byte pointer inside file */
+unsigned int byte;
+
+/** \brief locking flag which warrants mutual exclusion inside the monitor */
+pthread_mutex_t accessCR = PTHREAD_MUTEX_INITIALIZER;
+
+/** \brief flag which warrants that the data transfer region is initialized exactly once */
+pthread_once_t init = PTHREAD_ONCE_INIT;
+
+/**
+ *  \brief Initialization of the shared region.
+ *
+ *  Internal monitor operation.
+ */
+
+void initialization (void)
+{
+  fileId = byte = 0;                                        /* shared region filepointer and byte pointer are both 0 */
+}
+
+
+/**
+ *  \brief Insert the names of the files to be processed in an array.
+ *
+ *  Operation carried out by main.
+ *
+ *  \param listOfFiles names of files to process
+ *  \param size number of text files to be processed
+ */
+
+void presentDataFileNames(char* listOfFiles, unsigned int size){
+  
+  int i;
+  numbFiles = size;
+
+  for(i = 0; i < size; i++){
+    filesToProcess = malloc(strlen(listOfFiles[i]) + 1);
+    strcpy(filesToProcess,listOfFiles[i]);
+  }
+
+}
+
+
+/**
+ *  \brief Store a value in the data transfer region.
+ *
+ *  Operation carried out by the producers.
+ *
+ *  \param prodId producer identification
+ *  \param val value to be stored
+ */
+
+bool getAPieceOfData (unsigned int workerId, char *dataToBeProcessed, controlInfo)
+{
+  bool hasData = false;
+  if ((statusWorkers[workerId] = pthread_mutex_lock (&accessCR)) != 0)                                   /* enter monitor */
+     { errno = statusWorkers[workerId];                                                            /* save error in errno */
+       perror ("error on entering monitor(CF)");
+       statusWorkers[workerId] = EXIT_FAILURE;
+       pthread_exit (&statusWorkers[workerId]);
+     }
+  pthread_once (&init, initialization);                                              /* internal data initialization */
+
+  mem[fileId] = val;                                                                          /* store value in the FIFO */
+  fileId = (fileId + 1) % K;
+
+
+  if ((statusWorkers[workerId] = pthread_mutex_unlock (&accessCR)) != 0)                                  /* exit monitor */
+     { errno = statusWorkers[workerId];                                                            /* save error in errno */
+       perror ("error on exiting monitor(CF)");
+       statusWorkers[workerId] = EXIT_FAILURE;
+       pthread_exit (&statusWorkers[workerId]);
+     }
+return hasData;   
+}
+
+/**
+ *  \brief Get a value from the data transfer region.
+ *
+ *  Operation carried out by the consumers.
+ *
+ *  \param consId consumer identification
+ *
+ *  \return value
+ */
+
+void getVal (unsigned int workerId, controlInfo)
+{
+  unsigned int val;                                                                               /* retrieved value */
+
+  if ((statusWorkers[workerId] = pthread_mutex_lock (&accessCR)) != 0)                                   /* enter monitor */
+     { errno = statusWorkers[workerId];                                                            /* save error in errno */
+       perror ("error on entering monitor(CF)");
+       statusWorkers[workerId] = EXIT_FAILURE;
+       pthread_exit (&statusWorkers[workerId]);
+     }
+
+  val = mem[ri];                                                                   /* retrieve a  value from the FIFO */
+  ri = (ri + 1) % K;
+
+
+  if ((statusWorkers[workerId] = pthread_mutex_unlock (&accessCR)) != 0)                                   /* exit monitor */
+     { errno = statusWorkers[workerId];                                                             /* save error in errno */
+       perror ("error on exiting monitor(CF)");
+       statusWorkers[workerId] = EXIT_FAILURE;
+       pthread_exit (&statusWorkers[workerId]);
+     }
+
+  return val;
+}
