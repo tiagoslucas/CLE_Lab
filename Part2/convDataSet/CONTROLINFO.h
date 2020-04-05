df945917f8477ed8e4cb1c62dff2b96a0665b75c
FranciscoSantos97 <56774420+FranciscoSantos97@users.noreply.github.com>
2020-04-05 18:50:49 +0100

    Update CONTROLINFO.h

--- Part2/convDataSet/CONTROLINFO.h
+++ Part2/convDataSet/CONTROLINFO.h
@@ -1,14 +1,17 @@
-#include <stdlib.h>
-
 #ifndef CONTROLINFO_H
 #define CONTROLINFO_H
 
+#include <stdlib.h>
+#include <stdbool.h>
+#include "probConst.h"
+
 typedef struct
 {
+   bool initial;
    size_t filePosition;
    size_t numbSamples;
    size_t rxyIndex;
-   double result[];
-}CONTROLINFO;
+   double *result;
+} CONTROLINFO;
 
 #endif /* end of include guard: CONTROLINFO_H */
