aec32e6455dc97a9db12092f925647347aff6b2c
Tiago Lucas <tiago.slucas@gmail.com>
2020-04-06 01:55:41 +0100

    Presentation file added

--- Part1/probConst.h
+++ Part1/probConst.h
@@ -14,7 +14,7 @@
 /* Generic parameters */
 
 /** \brief number of worker Threads */
-#define  NUMB_THREADS       4
+#define  NUMB_THREADS       2
 
 /** \brief number of bytes to be processed each iteration */
 #define  K                  1024
