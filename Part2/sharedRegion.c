c88fd44c66427a2c6bd422e63b550df3c7925c2a
FranciscoSantos97 <56774420+FranciscoSantos97@users.noreply.github.com>
2020-04-06 06:49:54 +0100

    Update sharedRegion.c

--- Part2/sharedRegion.c
+++ Part2/sharedRegion.c
@@ -35,7 +35,11 @@ extern int statusWorkers[NUMB_THREADS];
 char* filesToProcess[MAX_FILES];
 
 /** \brief array of results read for each file file */
-FILEINFO *results;
+FILEINFO *filesManager;
+
+double **resultsMatrix;
+
+double **calculatedResultsMatrix;
 
 /** \brief number of files to process */
 unsigned int numbFiles;
@@ -66,7 +70,9 @@ bool isValidStopCharacter(char);
 void initialization (void)
 {
   filePosition = 0;                                        /* shared region filepointer and byte pointer are both 0 */
-  results = (FILEINFO*)malloc(sizeof(FILEINFO)*numbFiles);
+  filesManager = (FILEINFO*)malloc(sizeof(FILEINFO)*numbFiles);
+  resultsMatrix = malloc(numbFiles*DEFAULT_SIZE_SIGNAL*sizeof(double));
+  calculatedResultsMatrix = malloc(numbFiles*DEFAULT_SIZE_SIGNAL*sizeof(double));
   filePointer = NULL;
 }
 
@@ -116,40 +122,44 @@ bool getAPieceOfData(unsigned int workerId, double *x, double *y, CONTROLINFO *c
     }
     return false;
   }
-  
-  if(filePointer == NULL && ci->filePosition != filePosition){
+
+  if(ci->filePosition != filePosition && !ci->processing){
+    
     filePointer = fopen(filesToProcess[filePosition], "rb");
     size_t samples;
     size_t i = fread(&samples, sizeof(int), 1, filePointer);
-    printf("samples - %i\n", samples);
-    ci->rxyIndex = 0;
-    ci->numbSamples = samples;
 
-    FILEINFO *f = (FILEINFO*) malloc( sizeof(FILEINFO) + sizeof(double [ci->numbSamples]));
-    fread(x, sizeof(double), samples, filePointer);
-    fread(y, sizeof(double), samples, filePointer);
-    fread(f->expected, sizeof(double), samples, filePointer);
-
-    fclose(filePointer);
-
-    if(!ci->initial){
-      if(samples > ci->numbSamples){
-        x = (double*)malloc(sizeof(double)*samples);
-        y = (double*)malloc(sizeof(double)*samples);
-      }
-    }else{
-      if(samples > DEFAULT_SIZE_SIGNAL){
-        x = (double*)malloc(sizeof(double)*samples);
-        y = (double*)malloc(sizeof(double)*samples);
-      }
+    if(samples > ci->numbSamples){
+      x = (double*)malloc(sizeof(double)*samples);
+      y = (double*)malloc(sizeof(double)*samples);
     }
     
-    results[filePosition].filePosition = filePosition;
-    results[filePosition].numbSamples = ci->numbSamples;
+    ci->numbSamples = samples;
     ci->filePosition = filePosition;
-    ci->initial = false;
+    ci->processing = true;
+    ci->rxyIndex = 0;
 
-    printf("X - %f\tY - %f\tR - %f\n", &x[0], &y[0], &f->expected[0]);
+    double temp[samples];
+    
+
+    FILEINFO f ;
+    fread(x, sizeof(double), samples, filePointer);
+    fread(y, sizeof(double), samples, filePointer);
+    fread(temp, sizeof(double), samples, filePointer);
+    fclose(filePointer);
+
+    f.rxyIndex = 0;
+    f.filePosition = filePosition;
+    f.numbSamples = ci->numbSamples;
+    filesManager[filePosition] = f;
+    printf("value - %f\n", temp[0]);
+
+    /*for (size_t t = 0; t < samples; t++)
+    {
+      resultsMatrix[t][filePosition] = temp[t];
+    }*/
+    
+    
   }
 
 
@@ -185,13 +195,17 @@ void savePartialResults(unsigned int workerId, CONTROLINFO *ci)
     pthread_exit (&statusWorkers[workerId]);
   }
 
-
-  results[filePosition].result[ci->rxyIndex] = ci->result;
-  results[filePosition].rxyIndex++;
-
-  if(results[ci->filePosition].rxyIndex == results[ci->filePosition].numbSamples)
-    filePosition++;
+  calculatedResultsMatrix[ci->filePosition][ci->rxyIndex] = ci->result;
+  filesManager[ci->filePosition].rxyIndex++;
+  ci->rxyIndex = filesManager[ci->filePosition].rxyIndex;
   ci->result = 0;
+  if(filesManager[ci->filePosition].rxyIndex == filesManager[ci->filePosition].numbSamples){
+    filePosition++;
+    ci->filePosition = filePosition;
+    ci->processing = false;
+  }
+  
+  
 
 
   if ((statusWorkers[workerId] = pthread_mutex_unlock (&accessR)) != 0)                                   /* exit monitor */
@@ -209,9 +223,10 @@ void printResults(){
 
   for (i = 0; i < numbFiles; i++){
     numbErrors = 0;
-    for (x = 0; x < results[i].numbSamples; x++){
-      if (results[i].result[x] != results[i].expected[x]) {
+    for (x = 0; x < filesManager[i].numbSamples; x++){
+      if (resultsMatrix[i][x] != calculatedResultsMatrix[i][x]) {
           numbErrors++;
+          printf("x- %i,y- %i\nmine-%f\treal %f\n",i,x, resultsMatrix[i][x], calculatedResultsMatrix[i][x]);
       }
     }
     if(numbErrors==0)
@@ -220,5 +235,7 @@ void printResults(){
       printf("File %s had %i errors in total.\n", filesToProcess[i], numbErrors);
   }
   
-  free(results);
+  free(resultsMatrix);
+  free(calculatedResultsMatrix);
+  free(filesManager);
 }
